eb6f67df1eef69bc55e572930c601fa570347fc3
jacobjdavey <jacob.j.davey@vanderbilt.edu>
2026-02-21 17:14:36 -0600

    fixed startup logic

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/static/',
+  base: command === 'build' ? '/static/' : '/',
   build: {
     outDir: 'dist',
     sourcemap: false,
     assetsDir: 'assets',
   },
-})
+}))
